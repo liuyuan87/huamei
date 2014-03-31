c63168043131b5ca08d5af92f93f8123ec814a9c
pathfinder <chm2920@gmail.com>
2014-03-31 22:22:52 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Huamei::Application.config.secret_key_base = 'b730f7dfd155811d9c3660dcbaec86aac2c8994bd62151c45591c2affc14d17852d0d7a6b94b6819f81d05d1e759449b237c87e22eec52db83990da504b3c537'
