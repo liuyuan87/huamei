da21da54d2a2d6407663b4fb829929b80176bb33
pathfinder <chm2920@gmail.com>
2014-05-07 00:28:53 +0800

    夜色&magazine detail

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Huamei::Application.routes.draw do
   get 'case/(:id)' => 'start#case_info'
   get 'night' => 'start#night'
   get 'magazine' => 'start#magazine'
+  get 'magazine/(:id)' => 'start#magazine_detail'
   get 'contact' => 'start#contact'
   
   
