4de8902707bacab96ffa467c8d3402c6deee4078
pathfinder <chm2920@gmail.com>
2014-05-19 23:15:21 +0800

    update

---
+++ bbs/data/sysdata/cache_adminextend.php
@@ -0,0 +1,8 @@
+<?php
+//Discuz! cache file, DO NOT modify me!
+//Identify: f49cf2e82e2cfaa60af1abcfec29c6ad
+
+$adminextend = array (
+  0 => 'cloud.php',
+);
+?>
---
+++ bbs/data/sysdata/cache_pluginsetting.php
@@ -0,0 +1,7 @@
+<?php
+//Discuz! cache file, DO NOT modify me!
+//Identify: 9da4cc2fbbe4d5ffe635d3e98dd98d36
+
+$pluginsetting = array (
+);
+?>
