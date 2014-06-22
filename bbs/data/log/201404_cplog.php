315ed16f06d496324873e2265e26fef6b22891eb
root <root@AY140403151703053234Z.(none)>
2014-06-22 13:49:52 +0800

    delete bbs

--- bbs/data/log/201404_cplog.php
+++
@@ -1,4 +0,0 @@
-<?PHP exit;?>	1397837076	admin	1	113.31.35.15	cloudaddons	GET={extra=ac=tour&amp;view=installed; }; POST={};
-<?PHP exit;?>	1397837084	admin	1	113.31.35.15	cloudaddons	GET={frames=yes; extra=ac=tour&amp;view=installed; }; POST={};
-<?PHP exit;?>	1397837085	admin	1	113.31.35.15	cloudaddons	GET={extra=ac=tour&amp;view=installed; }; POST={};
-<?PHP exit;?>	1397837219	admin	1	113.31.35.15	logout	GET={}; POST={};
--- bbs/uc_server/data/logs/201404.php
+++
@@ -1,1 +0,0 @@
-<?PHP exit;?>	admin	113.31.35.15	1397837183	login	succeed
